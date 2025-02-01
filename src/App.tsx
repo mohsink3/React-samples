b2893ee9e6c4f6444ab4ac77601d9c42a133bb0a
mohsin khan <mohsin21.kh@gmail.com>
2025-02-01 12:44:45 +0530

    courseprop commited

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 
 import './App.css'
+import CourseList from './components/AdvanceCourse';
 
 
-import LoginStatus from './components/LoginStatus';
+
+
 
 function App() {
+  let courses = [
+    {
+      id: 1,
+      title: "Advanced Web Design",
+      description: "HTML CSS Bootstrap Responsive Design",
+      duration: "60 days",
+    },
+    {
+      id: 2,
+      title: "React Frontend",
+      description: "JS TS React",
+      duration: "50 days",
+    },
+    {
+      id: 3,
+      title: "Adv Excel",
+      description: "Adv Excel",
+      duration: "30 days",
+    },
+    {
+      id: 4,
+      title: "Python",
+      description: "Python Advanced",
+      duration: "30 days",
+    },
+  ];
   
   return (
     <>
       <header>
       <h1>React Demos</h1>
       </header>
-      <LoginStatus />
+      <CourseListProps CourseList={courses}/>
     </>
   );
 }
