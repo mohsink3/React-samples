54d1e630d2d2bf8d30fd39eb6424125da5ef0e35
mohsin khan <mohsin21.kh@gmail.com>
2025-02-05 09:30:32 +0530

    done formating to article and article list

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,17 +1,17 @@
-interface ArticleProps{
+interface ArticleProps {
     title: string;
-    children:string;
+    children: string;
 }
 
-const Article =(props:ArticleProps)=>{
-    return(
+const Article = (props: ArticleProps) => {
+    return (
         <div className="card shadow-sm my-3">
- <div className="card-body">
- <h3 className="card-title">{props.title}</h3>
- <p className="card-text">{props.children}</p> 
- </div>
- </div>
- );
+            <div className="card-body">
+                <h3 className="card-title">{props.title}</h3>
+                <p className="card-text">{props.children}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Article;
