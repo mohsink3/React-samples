520429dee7366d88c67cdf76cee5a754fb5dcdc5
mohsin khan <mohsin21.kh@gmail.com>
2025-02-09 18:33:26 +0530

    productlistsearch

--- src/components/ProductListSearch.tsx
+++ src/components/ProductListSearch.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 interface Product {
   id:number;
   title: string;
+  price:number;
+  images:string;
   
 }
 
@@ -48,12 +50,12 @@ function ProductListSearch() {
         {
           filteredProducts.map((product) => (
             <div className="col">
-              <div className="card h-100" key={product}>
-                <img src={product.image} className="card-img-top" alt="..." />
+              <div className="card h-100" key={product.id}>
+                <img src={product.images} className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">{product.title}</h5>
-                  <p className="card-text">{product.Price}</p>
-                  <p className="card-text">{product.Category}<small className="text-muted">D</small></p>
+                  <p className="card-text">{product.price}</p>
+                  
                 </div>
               </div>
             </div>
