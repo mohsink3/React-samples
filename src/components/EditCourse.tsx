863ceaa16474b545f5969221bbc5362a2397903d
mohsin khan <mohsin21.kh@gmail.com>
2025-02-11 19:09:15 +0530

    changes

--- src/components/EditCourse.tsx
+++ src/components/EditCourse.tsx
@@ -4,11 +4,32 @@ import { useParams, Link } from "react-router-dom";
 
 const EditCourse =()=>{
 
-    const API_URL = "https://67a97e126e9548e44fc3c722.mockapi.io/Courses";
+   
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const { id } = useParams<{ id: string }>();
+
+
+    const ProductList = () => {
+        const [courses, setCourses] = useState("");
+        const [error, setError] = useState("");
+      
+        const fetchCoures = async () => {
+          const response = await fetch(`https://67a97e126e9548e44fc3c722.mockapi.io/Courses/${id}`);
+          console.log(response);
+      
+          if (response.ok == false) {
+            setError("Error while fetching the list of Products");
+          } else {
+            const data = await response.json();
+            setCourses(data);
+          }
+        };
+      
+        useEffect(() => {
+          fetchProducts();
+        }, [id]);
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
